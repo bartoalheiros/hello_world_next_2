e49ae7d4a3a071537e415995bdd74dcb4ed53c03
BartoAlheiros <alheirosb@gmail.com>
2024-02-22 11:58:40 -0300

    meta tags e title

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import { Inter } from "next/font/google";
   import styles from "@/styles/Home.module.css";
 
+import Head from 'next/head'  
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="container">
-      <h1>Hello World Next.js</h1>
+    <>
+    <Head>
+      <title>Página Principal</title>
+      <meta name="keywords" content="Roupas, Calçados, Boné"></meta>
+      <meta 
+        name="description"
+        content="Encontre a melhor roupa para você"
+      ></meta>
+    </Head>
+    <main>
+      <h1 className={styles.title}>Hello World Next.js</h1>
     </main>
+    </>
   );
 }
