fba5e397735fbe9cff2c520cdb5deab4f05fb61d
BartoAlheiros <alheirosb@gmail.com>
2024-02-22 16:47:41 -0300

    rotas dinamicas com dados

--- pages/todos/[todoId].js
+++ pages/todos/[todoId].js
@@ -2,7 +2,41 @@ import Link from "next/link"
 
 import { useRouter } from 'next/router'
 
-export default function Todo() {
+export async function getStaticProps(context) {
+
+  const {params} = context
+
+  const data = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${params.todoId}`,
+  )
+
+  const todo = await data.json()
+
+  return {
+    props: { todo },
+  }
+
+}
+
+export async function getStaticPaths() {
+
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/')
+
+  const data = await response.json()
+
+  const paths = data.map((todo) => {
+    return {
+      params: {
+        todoId: `${todo.id}`,
+      }
+    }
+  })
+
+  return { paths, fallback: false }
+
+}
+
+export default function Todo({ todo }) {
 
   const router = useRouter()
 
@@ -11,22 +45,23 @@ export default function Todo() {
   return (
     <>
       <Link href="/">Voltar</Link>
-      <h1>Exibindo o todo: {todoId}</h1>
+      <h1>Exibindo o todo: {todo.id}</h1>
+      <h3>Texto: {todo.title}</h3>
       <p>
         Comentário: la la la...
-        <Link href={`/todos/${todoId}/comments/1`}>
+        <Link href={`/todos/${todo.id}/comments/1`}>
           Detalhes
         </Link>
       </p>
       <p>
         Comentário: le le le...
-        <Link href={`/todos/${todoId}/comments/2`}>
+        <Link href={`/todos/${todo.id}/comments/2`}>
           Detalhes
         </Link>
       </p>
       <p>
         Comentário: li li li...
-        <Link href={`/todos/${todoId}/comments/3`}>
+        <Link href={`/todos/${todo.id}/comments/3`}>
           Detalhes
         </Link>
       </p>
