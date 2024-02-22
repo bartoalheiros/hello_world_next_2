54f81e8ba9abf4c04d75e038b35999213d96079d
BartoAlheiros <alheirosb@gmail.com>
2024-02-22 10:20:55 -0300

    navegacao completa na navbar utilizando componente navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from 'next/link';
+import Navbar from "@/components/Navbar";
 
 import { Inter } from "next/font/google";
   import styles from "@/styles/Home.module.css";
@@ -10,18 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="container">
-      <ul>
-        <li>
-          <Link href="/products">
-            Produtos
-          </Link>       
-        </li>
-        <li>
-          <Link href="/about">
-            Sobre nós
-          </Link>       
-        </li>
-      </ul>
+      <Navbar />
       <h1>Hello World Next.js</h1>
     </main>
   );
